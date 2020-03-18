4476b6bb8ae8849b275e1817d3a5ccec0929736b
ArnoGodartEphec <44059862+ArnoGodartEphec@users.noreply.github.com>
2020-03-18 10:15:42 +0100

    partie 2

---
+++ noobcooking/build/precache-manifest.b5b20419bf33fd3c212aa6d8fb711d5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1527aa4ab54ccd92762ad3c518d102c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef42080af315a72dd528",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5270194a92d2b406ce93",
+    "url": "/static/js/2.db3d2b93.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.db3d2b93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef42080af315a72dd528",
+    "url": "/static/js/main.e3ba1b57.chunk.js"
+  },
+  {
+    "revision": "eda0389703d3cbad0d6e",
+    "url": "/static/js/runtime-main.7f0eb083.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ noobcooking/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" class="w3-display-bottomleft w3-padding-large w3-opacity">
+      <header className="App-header">
+      <h1 class="w3-xxlarge">Noob Cooking</h1> 
+        </header>
+    </div>
+  );
+}
+
+export default App;
+
